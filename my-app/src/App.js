6b3376d339e8865620b6574233b8c26c63af4788
Denis Alfonso <cubanyeti@gmail.com>
2020-09-02 20:39:22 -0400

    Started creating nav bar for website
    added logo and nav links, changed colors of links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar } from 'reactstrap';
+import logo from './logo.png';
+import {
+  Collapse,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+          <div className="app-body">
+
+            <div className="header-nav">
+              <a href="https://www.denisalfonso.com/" ><img src={logo} className="logo" /> </a>
+
+              
+              <Nav className="top-nav">
+                <NavItem>
+                  <NavLink href="">Projects</NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <NavLink href="">About</NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <Button className="contact-button rounded-pill border-primary"><NavLink href="">Contact</NavLink></Button>
+                </NavItem>
+              </Nav>
+
+            </div>
+</div>
+        
+        
+        
+      
     </div>
   );
 }
